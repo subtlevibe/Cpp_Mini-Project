47dbd86903fabfc36d475b7522509b53baf9e67b
John Paul Fernandes <johnpaulfernandes18@gmail.com>
2024-09-10 00:26:27 +0530

    Reduced funtion write times

--- Project_code.cpp
+++ Project_code.cpp
@@ -82,43 +82,29 @@ class info: public details
         getline(cin>>ws, tbc);
     }
 
-    void putdata()
+    void displaydetails(string title, string gen, string t, float r)
+    {
+        cout<<"\n";
+        cout<<"Movie:           "<<title<<endl;
+        cout<<"Genre:           "<<gen<<endl;
+        cout<<"Time Length:     "<<t<<endl;
+        cout<<"Rating:          "<<r<<"/10";
+
+    }
+    
+    void compfordet()
     {
         if(tbc==title1)
-        {
-            cout<<"\n";
-            cout<<"Movie:       "<<title1<<endl;
-            cout<<"Genre:       "<<gen1<<endl;
-            cout<<"Time Lenght: "<<tl1<<endl;
-            cout<<"Rating:      "<<rat1<<"/10";
-        }
+            displaydetails(title1, gen1, tl1, rat1);
 
         else if(tbc==title2)
-        {
-            cout<<"\n";
-            cout<<"Movie:       "<<title2<<endl;
-            cout<<"Genre:       "<<gen2<<endl;
-            cout<<"Time Lenght: "<<tl2<<endl;
-            cout<<"Rating:      "<<rat2<<"/10";
-        }
+            displaydetails(title2, gen2, tl2, rat2);
 
         else if(tbc==title3)
-        {
-            cout<<"\n";
-            cout<<"Movie:       "<<title3<<endl;
-            cout<<"Genre:       "<<gen3<<endl;
-            cout<<"Time Lenght: "<<tl3<<endl;
-            cout<<"Rating:      "<<rat3<<"/10";
-        }
+            displaydetails(title3, gen3, tl3, rat3);
 
         else if(tbc==title4)
-        {
-            cout<<"\n";
-            cout<<"Movie:       "<<title4<<endl;
-            cout<<"Genre:       "<<gen4<<endl;
-            cout<<"Time Lenght: "<<tl4<<endl;
-            cout<<"Rating:      "<<rat4<<"/10";
-        }
+            displaydetails(title4, gen4, tl4, rat4);
     }
 };
 
@@ -128,22 +114,22 @@ class seats
 
     int avseats1=30, avseats2=30, avseats3=30, avseats4=30;
 
-    char seats1[200]={'A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'A9', 'A10', 'B1', 'B2', 'B3', 'B4', 'B5', 'B6', 'B7', 'B8', 'B9', 'B10', 'C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9', 'C10', 'D1', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7', 'D8', 'D9', 'D10'};
-    char seats2[30]={'A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'A9', 'A10', 'B1', 'B2', 'B3', 'B4', 'B5', 'B6', 'B7', 'B8', 'B9', 'B10', 'C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9', 'C10', 'D1', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7', 'D8', 'D9', 'D10'};
-    char seats3[30]={'A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'A9', 'A10', 'B1', 'B2', 'B3', 'B4', 'B5', 'B6', 'B7', 'B8', 'B9', 'B10', 'C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9', 'C10', 'D1', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7', 'D8', 'D9', 'D10'};
-    char seats4[30]={'A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'A9', 'A10', 'B1', 'B2', 'B3', 'B4', 'B5', 'B6', 'B7', 'B8', 'B9', 'B10', 'C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9', 'C10', 'D1', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7', 'D8', 'D9', 'D10'};
+    string seats1[40]={"A1", "A2", "A3", "A4", "A5", "A6", "A7", "A8", "A9", "A10", "B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9", "B10", "C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "D1", "D2", "D3", "D4", "D5", "D6", "D7", "D8", "D9", "D10"};
+    string seats2[40]={"A1", "A2", "A3", "A4", "A5", "A6", "A7", "A8", "A9", "A10", "B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9", "B10", "C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "D1", "D2", "D3", "D4", "D5", "D6", "D7", "D8", "D9", "D10"};
+    string seats3[40]={"A1", "A2", "A3", "A4", "A5", "A6", "A7", "A8", "A9", "A10", "B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9", "B10", "C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "D1", "D2", "D3", "D4", "D5", "D6", "D7", "D8", "D9", "D10"};
+    string seats4[40]={"A1", "A2", "A3", "A4", "A5", "A6", "A7", "A8", "A9", "A10", "B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9", "B10", "C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "D1", "D2", "D3", "D4", "D5", "D6", "D7", "D8", "D9", "D10"};
 
     public:
 
-    void displayseats(string seatsdis)
+    void displayseats(string seatsdis[])
     {
         cout<<"Available seats: ";
         cout<<"\n-----------------------------------------";
         cout<<"\t\t\tSCREEN";
         cout<<"\n-----------------------------------------";
 
-        for(int i=0;i<10;i++)
-        cout<<seatsdis[i]<<"\t\t\t";
+        for(int i=0;i<30;i++)
+        cout<<seatsdis[i]<<"\t";
     }
 };
 
@@ -154,9 +140,7 @@ class booking: public seats, public info
     int tickets=1;
     string tickname;
 
-    void displaydetails()
-    {
-        void displaymoviedetails(string title, int avseats, string date, string time)
+    void displaydetailsdetails(string title, int avseats, string date, string time, string seats[])
         {
             cout<<"\n";
             cout<<"Movie:           "<<title<<endl;
@@ -164,43 +148,44 @@ class booking: public seats, public info
             cout<<"Date:            "<<date<<endl;
             cout<<"Time:            "<<time;
 
-            cout<<"\nEnter number of teickets: ";
+            cout<<"\nEnter number of tickets: ";
             cin>>tickets;
 
             if(tickets>avseats)
             cout<<"Insufficient seats!";
 
             else
-            displayseats();
+            displayseats(seats);
         }
 
+    void compare()
+    {
         if(tbc==title1)
-        displaymoviedetails(title1, avseats1, date1, time1)
+        displaydetailsdetails(title1, avseats1, date1, time1, seats1);
 
         else if(tbc==title2)
-        displaymoviedetails(title2, avseats2, date2, time2)
+        displaydetailsdetails(title2, avseats2, date2, time2, seats2);
         
 
         else if(tbc==title3)
-        displaymoviedetails(title3, avseats3, date3, time3)
+        displaydetailsdetails(title3, avseats3, date3, time3, seats3);
         
 
         else if(tbc==title4)
-        displaymoviedetails(title4, avseats4, date4, time4)
+        displaydetailsdetails(title4, avseats4, date4, time4, seats4);
 
         else
         cout<<"\nMovie not found!";
     }
 
-    void asktickets()
+    /*void asktickets()
     {
         cout<<"\n\nHow many tickets do you want to purchase?: ";
         cin>>tickets;
 
         if()
-    }
-}
-}
+    }*/
+};
 
 void header()
 {
@@ -246,14 +231,16 @@ void mainmenu()
             {
                 i.titles();
                 i.getname();
-                b.displaydetails();
+                b.compare();
+
+                break;
             }
 
             case 3:
             {
                 i.titles();
                 i.getname();
-                i.putdata();
+                i.compfordet();
                 break;
             }
 
