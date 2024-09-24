08ab60c3e28bf9c46aa4093b9942406b86c73bd1
John Paul Fernandes <johnpaulfernandes18@gmail.com>
2024-09-25 00:17:41 +0530

    Log out and Cancel seat feature was added

--- Project_code.cpp
+++ Project_code.cpp
@@ -108,7 +108,7 @@ class LogIn: virtual public User
 
         if(flag==0)
         {
-            cout<<"\nNo such account with the username";
+            cout<<"\nNo such account with the username"<<endl;
             getuser();
         }
 
@@ -136,7 +136,7 @@ class LogIn: virtual public User
 
         else
         {
-            cout<<"\nIncorrect password!";
+            cout<<"\nIncorrect password!"<<endl;
             getpass();
         }
     }
@@ -200,6 +200,10 @@ protected:
 	static string seats2[100];
 	static string seats3[100];
 	static string seats4[100];
+
+	//Constant data member
+
+	const string backup[100]={ "A1", "A2", "A3", "A4", "A5", "A6", "A7", "A8", "A9", "A10", "B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9", "B10", "C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "D1", "D2", "D3", "D4", "D5", "D6", "D7", "D8", "D9", "D10", "E1", "E2", "E3", "E4", "E5", "E6", "E7", "E8", "E9", "E10", "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "G1", "G2", "G3", "G4", "G5", "G6", "G7", "G8", "G9", "G10", "H1", "H2", "H3", "H4", "H5", "H6", "H7", "H8", "H9", "H10", "I1", "I2", "I3", "I4", "I5", "I6", "I7", "I8", "I9", "I10", "J1", "J2", "J3", "J4", "J5", "J6", "J7", "J8", "J9", "J10" };
     
 public:
 
@@ -333,7 +337,7 @@ void bookseats(string s[], int t)
 	string seatnum;
 	int flag = 0;
 
-	for (static int i = 0; i<t; i++)
+	for (int i = 0; i<t; i++)
 	{
 		cout << "\n\nEnter seat number (A1, C10, D4...): ";
 		cin >> seatnum;
@@ -354,7 +358,7 @@ void bookseats(string s[], int t)
 		if (flag == 0)
 		{
 			cout << "Seat not found! Enter again!";
-			bookseats(s, t);
+			i--;
 		}
 
 		flag = 0;
@@ -377,8 +381,9 @@ class booking : public seats, public details
 {
 public:
 
-	int addtotickets = 1;
+	int totaltickets = 1;
 	string tickname;
+	string canname;
 
 	void gettickname()
 	{
@@ -468,20 +473,86 @@ public:
 	void getnumber()
 	{
 		cout << "\nEnter total number of tickets: ";
-		cin >> addtotickets;
+		cin >> totaltickets;
 
-		nooftickets(addtotickets);
+		nooftickets(totaltickets);
 	}
 
 	void checkseats(int &s, string seat[])
 	{
-		if (s<addtotickets)
+		if (s<totaltickets)
 			cout << "\nInsufficient seats!";
 
 		else
 		{
-			s = s - addtotickets;
-			displayseats(seat, addtotickets);
+			s = s - totaltickets;
+			displayseats(seat, totaltickets);
+		}
+	}
+
+	void getname()
+	{
+		cout<<"\nEnter name of the movie: ";
+		getline(cin>>ws, canname);
+
+		compcan(canname);
+	}
+
+	void compcan(string name)
+	{
+		if (name == title1)
+			cancel(avseats1, seats1);
+
+		else if (name == title2)
+			cancel(avseats2, seats2);
+
+		else if (name == title3)
+			cancel(avseats3, seats3);
+
+		else if (name == title4)
+			cancel(avseats4, seats4);
+
+		else
+        {
+			cout<<"\nMovie not found! Enter again!";
+			getname();
+		}
+	}
+
+	void cancel(int &av, string seat[])
+	{
+		string seatnum;
+		int num, flag=0;
+
+		cout<<"\nHow many seats do you want to cancel?\t";
+		cin>>num;
+
+		for(int i=0; i<num; i++)
+		{
+		    cout << "\n\nEnter seat number (A1, C10, D4...): ";
+		    cin>>seatnum;
+
+			for(int j=0; j<100 ;j++)
+			{
+				if((seat[j]=="Bk") && (backup[j]==seatnum))
+				{
+					seat[j]=backup[j];
+					flag=1;
+					break;
+				}
+			}
+		}
+
+		if(flag==0)
+		{
+		    cout<<"\nSeat not found or seat is not boooked! Enter again!";
+			i--;
+		}
+
+		else
+		{
+			av++;
+		    cout<<"\nSeat cancelled successfully!";
 		}
 	}
 };
@@ -582,6 +653,8 @@ void header()
 	int line = 24;
 	int space = 2;
 
+	cout<<"\n";
+
 	for (int i = 0; i < space; i++)
 		cout << ' ';
 
@@ -601,6 +674,8 @@ void header()
 	cout << "\n";
 }
 
+void account();
+
 void mainmenu()
 {
 	int choice;
@@ -608,35 +683,54 @@ void mainmenu()
 	booking b;
 	billing bill;
 
-	cout << "\n\n   --Dashboard--\n1. Display Shows\n2. Book a seat\n3. Movie Information\nSelect the number corresponding to your choice: ";
+	cout << "\n\n   --Dashboard--\n1. Display Shows\n2. Book a seat\n3. Movie Information\n4. Cancel a booked seat\n5. Log out\nSelect the number corresponding to your choice: ";
 	cin >> choice;
 
 	switch (choice)
 	{
-	case 1:
-	{   i.showsdisplay();
-	break;
-	}
+		case 1:
+		{
+			i.showsdisplay();
+			break;
+		}
 
-	case 2:
-	{
-		i.titles();
-		b.gettickname();
-		b.nooftickets();
-		bill.asksnacks();
-		bill.displaygrand();
-		break;
-	}
+		case 2:
+		{
+			i.titles();
+			b.gettickname();
+			b.nooftickets();
+			bill.asksnacks();
+			bill.displaygrand();
+			break;
+		}
 
-	case 3:
-	{
-		i.titles();
-		i.getname();
-		i.compfordet();
-		break;
-	}
+		case 3:
+		{
+			i.titles();
+			i.getname();
+			i.compfordet();
+			break;
+		}
 
-	default: cout << "\nInvalid choice!!";
+		case 4:
+		{
+			b.getname();
+			break;
+		}
+
+		case 5:
+		{
+			header();
+			account();
+			mainmenu();
+			break;
+		}
+
+		default:
+		{ 
+			cout << "\nInvalid choice!!";
+			mainmenu();
+	    }
 	}
 }
 
@@ -672,7 +766,7 @@ void account()
 
         default: 
         {
-            cout<<"\nInvalid choice!";
+            cout<<"\nInvalid choice!"<<endl;
             account();
         }
     }
